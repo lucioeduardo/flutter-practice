def685ade61993c7f26a59c559037915573cd946
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2022-04-25 10:19:59 -0300

    ⭐ : project creation and home initial layout

---
+++ foodfancy/android/app/src/main/kotlin/com/example/foodfancy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodfancy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ foodfancy/lib/ui/pages/home/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    const verticalSpace18 = SizedBox(height: 18);
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: ListView(
+        padding: const EdgeInsets.all(18),
+        children: [
+          const Text('New Request'),
+          verticalSpace18,
+          const Placeholder(
+            fallbackHeight: 217,
+          ),
+          verticalSpace18,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Column(
+                  children: const [
+                    Placeholder(fallbackHeight: 163),
+                    Placeholder(fallbackHeight: 163),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 22),
+              Flexible(
+                child: Column(
+                  children: const [
+                    SizedBox(height: 30),
+                    Placeholder(fallbackHeight: 163),
+                    Placeholder(fallbackHeight: 163),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
